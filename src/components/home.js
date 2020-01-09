ac0699278407770601ac44b3945f5fd28fac6cd3
soniapathak <sonia.soni04@gmail.com>
2020-01-09 13:36:56 +0100

    fetch users data

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getUsers } from "../components/actions/index";
 
 class Home extends Component {
   state = {};
+
+  componentDidMount() {
+    console.log("component did mount");
+    this.props.getUsers();
+  }
   render() {
     return (
       <div>
         <h1>Welcome to User's reopository</h1>
+        
       </div>
     );
   }
 }
 
-export default Home;
+export default connect(null, { getUsers })(Home);
