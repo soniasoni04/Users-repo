966ee26f31b8350c05c6a9341ffdde0060480051
soniapathak <sonia.soni04@gmail.com>
2020-01-09 22:38:11 +0100

    completed

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,7 +3,6 @@ import { GET_USERS } from "../actions/index";
 export default function(state = [], action = {}) {
   switch (action.type) {
     case GET_USERS:
-      console.log("users data: ", action.payload);
       return [...action.payload];
 
     default:
